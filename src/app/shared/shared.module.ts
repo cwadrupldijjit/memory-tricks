a2e4274622b5d90d85a0957dd217261bcce2428e
Samuel Skeen <sskeen@sskeendevelopment.com>
2019-06-11 17:44:05 -0600

    updating many dependencies

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  MatCardModule,
-  MatDialogModule,
-  MatButtonModule,
-  MatRadioModule,
-  MatInputModule,
-} from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { PersistenceDirective } from './persistence.directive';
 import { UserInterfaceModule } from '../user-interface/user-interface.module';
 
--- src/app/user-interface/user-interface.module.ts
+++ src/app/user-interface/user-interface.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
-import { MatCardModule, MatDialogModule, MatButtonModule, MatRadioModule, MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
